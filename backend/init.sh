6f95c7084da0ca39034b32bf68cd7a77705d536a
aaghbal <aaghbal@student.1337.ma>
2024-09-22 21:45:03 +0100

    fix: database with migration

--- backend/init.sh
+++ backend/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sleep 10
+sleep 15
 
-# python manage.py makemigrations
+python manage.py makemigrations
 
-# python manage.py migrate
+python manage.py migrate
 
 python manage.py runserver 0.0.0.0:8000

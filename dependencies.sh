18165ce37f39ed7ac23761eb7f1b65b1ea753989
Abdellatyf En-Neiymy <aben-nei@e1r10p15.1337.ma>
2024-09-27 13:12:23 +0100

    add some dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,5 @@
 npm install next@latest react@latest react-dom@latest .
-npm install -D tailwindcss postcss autoprefixer
+npm install -D tailwindcss tailwind-scrollbar framer-motion postcss autoprefixer
 npm install react-icons
 npm install -g json-server
 npm install emoji-mart

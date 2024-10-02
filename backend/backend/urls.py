51f0000571683e28c0f304053a319c39b3542bbf
Mohamed El Youssfi <mohamedyoussfi20@gmail.com>
2024-10-02 15:08:11 +0100

    before merging with auth

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from authentication import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'users', views.user_viewsets, basename='user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]

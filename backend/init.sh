35550d88093b9cb905d32974b654220ced10984e
aaghbal <aaghbal@student.1337.ma>
2024-11-05 19:18:57 +0100

    fix: send logs backend from django to logstash in script

--- backend/init.sh
+++ backend/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python manage.py makemigrations authentication game chat
-
-python manage.py migrate
-
-python manage.py runserver 0.0.0.0:8000
+{
+  python manage.py makemigrations authentication game chat
+  python manage.py migrate
+  python manage.py runserver 0.0.0.0:8000
+} 2>&1 | nc -u logstash 5044

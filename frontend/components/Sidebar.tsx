8e64bffa94b3f995933c3d6b7ab8fbb2aaa3e2d5
Abdellatyf En-Neiymy <aben-nei@e2r9p14.1337.ma>
2024-11-10 20:16:22 +0100

    before merging with Chat

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -24,12 +24,12 @@ function Sidebar() {
     const home = pathname === '/';
     return (
         <nav className='w-full sm:w-[100px] sm:h-full h-16 flex sm:flex-col items-center justify-around sm:justify-center sm:gap-[5%]'>
-            <NavLink href='/' isActive={home}><TbLayoutDashboardFilled title='dashboard' className='text-white w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] hover:text-opacity-100 transition'/></NavLink>
-            <NavLink href='/game' isActive={game}><IoLogoGameControllerB title='game' className='text-white w-[30px] h-[30px] sm:w-[40px] sm:h-[40px]'/></NavLink>
-            <NavLink href='/tournament' isActive={tournament}><GiLaurelsTrophy title='tournament' className='text-white w-[30px] h-[30px] sm:w-[40px] sm:h-[40px]'/></NavLink>
-            <NavLink href='/profile' isActive={profile}><FaUser title='profile' className='text-white w-[30px] h-[30px] sm:w-[40px] sm:h-[40px]'/></NavLink>
-            <NavLink href='/chat' isActive={chat}><IoChatbubblesSharp title='chat' className='text-white w-[30px] h-[30px] sm:w-[40px] sm:h-[40px]'/></NavLink>
-            <NavLink href='/settings' isActive={settings}><IoSettings title='settings' className='text-white w-[30px] h-[30px] sm:w-[40px] sm:h-[40px]'/></NavLink>
+            <NavLink href='/' isActive={home}><TbLayoutDashboardFilled title='Dashboard' className='text-white w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] hover:text-opacity-100 transition'/></NavLink>
+            <NavLink href='/game' isActive={game}><IoLogoGameControllerB title='Game' className='text-white w-[30px] h-[30px] sm:w-[40px] sm:h-[40px]'/></NavLink>
+            <NavLink href='/tournament' isActive={tournament}><GiLaurelsTrophy title='Tournament' className='text-white w-[30px] h-[30px] sm:w-[40px] sm:h-[40px]'/></NavLink>
+            <NavLink href='/profile' isActive={profile}><FaUser title='Profile' className='text-white w-[30px] h-[30px] sm:w-[40px] sm:h-[40px]'/></NavLink>
+            <NavLink href='/chat' isActive={chat}><IoChatbubblesSharp title='Chat' className='text-white w-[30px] h-[30px] sm:w-[40px] sm:h-[40px]'/></NavLink>
+            <NavLink href='/settings' isActive={settings}><IoSettings title='Settings' className='text-white w-[30px] h-[30px] sm:w-[40px] sm:h-[40px]'/></NavLink>
         </nav>
   )
 }

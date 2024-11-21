ebbc4830e9076caf4b33d7ae7706027dc8f23670
ABDELLATYF EN-NEIYMY <rca8272@gmail.com>
2024-11-21 03:00:09 +0100

    update next.config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,6 +14,10 @@ module.exports = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+      }
     ],
   },
   reactStrictMode: false, // Disable React Strict Mode

6f125a62bd6b1e1a675316c72486bccf233b32cd
Abdellatyf En-Neiymy <aben-nei@e2r9p7.1337.ma>
2024-12-02 22:12:04 +0100

    working on image upload

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link'
 import axios from 'axios'
 import { useRouter } from 'next/navigation';
@@ -13,18 +13,46 @@ export default function Home() {
     const {setIsAuthenticated, fetchAuthUser, setauthUser} = useContext(UserContext);
 
     return (
-            <div className='
-                flex
-                w-full
-                justify-center
-                items-center
-                h-screen
-                text-white'>
-            <p>
-                upload
-            </p>
-            <input type="file" name="file" id="file" className="hidden" />
-            {/* <input type="image" name="file" id="file" className="hidden" /> */}
-            </div>
+        <div
+        className="
+          flex
+          flex-col
+          w-full
+          justify-center
+          items-center
+          h-screen
+          text-white"
+      >
+        <input type="file" name="file" id="file" />
+        <button
+          onClick={async () => {
+            const fileInput = document.getElementById('file') as HTMLInputElement;
+            if (!fileInput || !fileInput.files || fileInput.files.length === 0) {
+              alert('Please select a file first!');
+              return;
+            }
+  
+            const file = fileInput.files[0];
+            const formData = new FormData();
+            formData.append('file', file);
+  
+            try {
+                const cookies = await getCookies();
+                const csrfToken = cookies.cookies.csrftoken;
+              const response = await axios.post('http://localhost:8000/api/auth/upload/', formData, {
+                headers: {
+                  'Content-Type': 'multipart/form-data',
+                    'X-CSRFToken': csrfToken,
+                },
+              });
+  
+              console.log('Uploaded file URL:', response.data.url);
+            } catch (error) {
+            }
+          }}
+        >
+          Upload
+        </button>
+      </div>
     )
 }

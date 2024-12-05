c4d8072f098c141790e6cce6ee42c26093644c68
Mohamed El Youssfi <mohamedyoussfi20@gmail.com>
2024-12-05 16:39:32 +0100

    refactor: notification component

--- frontend/components/NotificationElement.tsx
+++ frontend/components/NotificationElement.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { AiFillMessage } from "react-icons/ai";
+import { FaUserFriends } from "react-icons/fa";
 
 type Props = {
-    
+    Url: string,
+    Avatar: string,
+    Description: string,
+    Date: string,
+    Key: string,
+    NotifType: string,
 }
 
-function NotificationElement({}: Props) {
+function NotificationElement({Url, Avatar, Description, Date, Key, NotifType}: Props) {
   return (
-    <div>NotificationElement</div>
+    <Link href={Url} key={Key} className='w-full p-4 flex gap-3 hover:cursor-pointer hover:bg-white/5'>
+      <div className='w-[60px] h-[60px] rounded-full'>
+        <Image className='rounded-full' width={60} height={60} src={Avatar} alt='user_image'></Image>
+      </div>
+      <div className='flex flex-col gap-3'>
+        <span className='text-[18px]'>{Description}</span>
+        <span className='text-[15px] text-white/50'>{Date}</span>
+          {/* <div className='flex gap-2'>
+            <button onClick={() => handleAccept(notification?.friend_request)} className='w-[110px] h-[45px] bg-[#436850] hover:bg-[#538264] rounded-[30px] text-[18px]'>Accept</button>
+            <button onClick={() => handleReject(notification?.friend_request)} className='w-[110px] h-[45px] bg-[#c75462] hover:bg-[#d75b69] rounded-[30px] text-[18px]'>Reject</button>
+          </div> */}
+      </div>
+      <div className='w-[45px] h-[45px]'>
+        { NotifType === 'message' && <AiFillMessage className='text-white w-[45px] h-[45px]' /> }
+        { NotifType === 'friend_request' && <FaUserFriends className='text-white w-[45px] h-[45px]' /> }
+      </div>
+  </Link>
   )
 }
 
--- frontend/components/NotificationMenu.tsx
+++ frontend/components/NotificationMenu.tsx
@@ -8,6 +8,7 @@ import { toast } from 'react-toastify';
 import { useState } from 'react';
 import Link from 'next/link'
 import { AiFillMessage } from "react-icons/ai";
+import NotificationElement from './NotificationElement'
 
 function NotificationMenu() {
 
@@ -91,24 +92,12 @@ function NotificationMenu() {
           </div>
           <div className='h-[calc(100%_-_65px)] w-full scrollbar-none overflow-y-auto scroll-smooth'>
             {notifications?.map((notification, index) => {
-              return (
-                <Link href='/friend-requests/' key={index} className='w-full p-4 flex gap-3 hover:cursor-pointer hover:bg-white/5'>
-                  <div className='w-[60px] h-[60px] rounded-full'>
-                    <Image className='rounded-full' width={60} height={60} src={notification?.sender_info?.avatar_url} alt='user_image'></Image>
-                  </div>
-                  <div className='flex flex-col gap-3'>
-                    <span className='text-[18px]'>{notification?.description}</span>
-                    <span className='text-[15px] text-white/50'>{notification?.get_human_readable_time}</span>
-                    {/* <div className='flex gap-2'>
-                      <button onClick={() => handleAccept(notification?.friend_request)} className='w-[110px] h-[45px] bg-[#436850] hover:bg-[#538264] rounded-[30px] text-[18px]'>Accept</button>
-                      <button onClick={() => handleReject(notification?.friend_request)} className='w-[110px] h-[45px] bg-[#c75462] hover:bg-[#d75b69] rounded-[30px] text-[18px]'>Reject</button>
-                    </div> */}
-                  </div>
-                  <div className='w-[45px] h-[45px]'>
-                    <AiFillMessage className='text-white w-[45px] h-[45px]' />
-                  </div>
-                </Link>
-              )
+              if (notification.notif_type === 'friend_request') {
+                return (<NotificationElement Url='/friend-requests/' Avatar={notification?.sender_info.avatar_url} Description={notification?.description} Date={notification.date} Key={index} NotifType={notification?.notif_type} />)
+              }
+              else if (notification.notif_type === 'message') {
+                return (<NotificationElement Url='/messages/' Avatar={notification?.sender_info.avatar_url} Description={notification?.description} Date={notification?.date} Key={index} NotifType={notification?.notif_type} />)
+              }
             })}
           </div>
         </div>

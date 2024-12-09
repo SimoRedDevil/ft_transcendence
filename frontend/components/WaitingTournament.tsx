8544146edd06e128cc1058978429a828c2edf851
aaghbal <aaghbal@student.1337.ma>
2024-12-09 15:53:33 +0100

    feat: responsive of wiatingTournament component

--- frontend/components/WaitingTournament.tsx
+++ frontend/components/WaitingTournament.tsx
@@ -13,14 +13,27 @@ export default function WaitingTournament({ numberplayer }: WaitingTournamentPro
     setNumplayer(numberplayer);
   }, [numberplayer]);  
   return (
-    <div className="flex justify-center items-center bg-[url('/images/waitingimage.png')] w-[800px] h-[550px] bg-cover bg-center border border-white border-opacity-30 rounded-[30px] space-x-3 opacity-90">
-        <div className={`bg-[url('/images/avta.png')] w-[100px] h-[100px] border ${ numplayer >= 1 ? 'border-yellow-300': 'border-black opacity-40'} bg-cover bg-center rounded-[100px]`}>
+    <div className="aspect-[4/3] flex justify-center items-center bg-[url('/images/waitingimage.png')] flex-col lm:flex-row space-y-3 lm:space-y-0 lm:space-x-3
+                    3xl:w-[800px] 2xl:w-[700px] xl:w-[600px] lm:w-[500px] ls:w-[400px] w-[300px] bg-cover bg-center border border-white border-opacity-30 rounded-[30px]  opacity-90">
+        <div className='flex justify-center items-center space-x-3 '>
+            <div className={`bg-[url('/images/avta.png')] 2xl:w-[100px] 2xl:h-[100px] xl:w-[80px] xl:h-[80px] lm:w-[70px] lm:h-[70px]
+                              ls:w-[60px] ls:h-[60px] w-[50px] h-[50px]
+                             border ${ numplayer >= 1 ? 'border-yellow-300': 'border-black opacity-40'} bg-cover bg-center rounded-[100px]`}>
+            </div>
+            <div className={`bg-[url('/images/avta.png')] 2xl:w-[100px] 2xl:h-[100px] xl:w-[80px] xl:h-[80px] lm:w-[70px] lm:h-[70px]
+                              ls:w-[60px] ls:h-[60px] w-[50px] h-[50px]
+                              border ${ numplayer >= 2 ? 'border-yellow-300': 'border-black opacity-40'} bg-cover bg-center rounded-[100px]`}>
+            </div>
         </div>
-        <div className={`bg-[url('/images/avta.png')] w-[100px] h-[100px] border ${ numplayer >= 2 ? 'border-yellow-300': 'border-black opacity-40'} bg-cover bg-center rounded-[100px]`}>
-        </div>
-        <div className={`bg-[url('/images/avta.png')] w-[100px] h-[100px] border ${ numplayer >= 3 ? 'border-yellow-300': 'border-black opacity-40'} bg-cover bg-center rounded-[100px]`}>
-        </div>
-        <div className={`bg-[url('/images/avta.png')] w-[100px] h-[100px] border ${ numplayer >= 4 ? 'border-yellow-300': 'border-black opacity-40'} bg-cover bg-center rounded-[100px]`}>
+        <div className='flex justify-center items-center space-x-3'>
+            <div className={`bg-[url('/images/avta.png')] 2xl:w-[100px] 2xl:h-[100px]
+                              xl:w-[80px] xl:h-[80px] lm:w-[70px] lm:h-[70px] ls:w-[60px] ls:h-[60px] w-[50px] h-[50px]
+                              border ${ numplayer >= 3 ? 'border-yellow-300': 'border-black opacity-40'} bg-cover bg-center rounded-[100px]`}>
+            </div>
+            <div className={`bg-[url('/images/avta.png')] 2xl:w-[100px] 2xl:h-[100px]
+                              xl:w-[80px] xl:h-[80px] lm:w-[70px] lm:h-[70px] ls:w-[60px] ls:h-[60px] w-[50px] h-[50px]
+                             border ${ numplayer >= 4 ? 'border-yellow-300': 'border-black opacity-40'} bg-cover bg-center rounded-[100px]`}>
+            </div>
         </div>
     </div>
   )
